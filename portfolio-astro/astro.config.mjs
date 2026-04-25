0511d0e193670bddd694495d3094c915ee3f9850
LucasA21 <lucasaraya19@hotmail.com>
2026-04-25 20:11:39 -0300

    feat: :zap: update SEO config

--- portfolio-astro/astro.config.mjs
+++ portfolio-astro/astro.config.mjs
@@ -8,6 +8,8 @@ import { loadEnv } from 'vite';
 
 import vercel from '@astrojs/vercel';
 
+import sitemap from '@astrojs/sitemap';
+
 const { PUBLIC_SANITY_PROJECT_ID, PUBLIC_SANITY_DATASET } = loadEnv(
   process.env.NODE_ENV || "development",
   process.cwd(),
@@ -16,20 +18,19 @@ const { PUBLIC_SANITY_PROJECT_ID, PUBLIC_SANITY_DATASET } = loadEnv(
 
 
 export default defineConfig({
+  site: 'https://lucasaraya.com.ar',
   vite: {
     plugins: [tailwindcss()]
   },
 
-  integrations: [
-    react(),
-    icon(),
-    sanity({
-      projectId: PUBLIC_SANITY_PROJECT_ID,
-      dataset: PUBLIC_SANITY_DATASET,
-      useCdn: false,
-      studioBasePath: '/admin',
-    }),
-  ],
+  integrations: [react(), icon(), sanity({
+    projectId: PUBLIC_SANITY_PROJECT_ID,
+    dataset: PUBLIC_SANITY_DATASET,
+    useCdn: false,
+    studioBasePath: '/admin',
+  }), sitemap({
+    filter: (page) => !page.includes('/admin')
+  })],
 
   adapter: vercel(),
 });
